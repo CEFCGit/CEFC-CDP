b0f6e9b056921c640ba6747d89cf2f26d0d06e69
YolindaMoodley <38342306+YolindaMoodley@users.noreply.github.com>
2018-06-19 15:25:35 +1000

    Added to get schema name based on max creation date

--- Build/Stored Procedures/core.Purge_Backup_CDP_Tables.sql
+++ Build/Stored Procedures/core.Purge_Backup_CDP_Tables.sql
@@ -1,9 +1,10 @@
-/****** Object:  StoredProcedure [core].[Purge_Backup_CDP_Tables]    Script Date: 19/06/2018 10:19:10 AM ******/
+/****** Object:  StoredProcedure [core].[Purge_Backup_CDP_Tables]    Script Date: 19/06/2018 12:23:30 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 -- ==========================================================================
@@ -24,19 +25,26 @@ declare @tabname nvarchar(100)
 --Drop Tables
 --===============================================================
 declare table_cursor cursor for 
-		select	bk_sh.name as schema_name,
-				st.name as table_name					
-			from sys.tables st
-			inner join (select distinct name,
-			             schema_id 
-						from sys.schemas
-						where name like 'backup_%'
-						and schema_id < (select max(schema_id)
-										from sys.schemas
-										 where name like 'backup_%'
-										 )
-						) as bk_sh
-				on st.schema_id = bk_sh.schema_id
+		select	s.name,
+				st.name
+		from sys.tables st
+		join sys.schemas s
+			on st.schema_id = s.schema_id
+		where s.name <> (
+							select sn.name
+							from sys.schemas sn
+							join sys.tables tab
+								on sn.schema_id = tab.schema_id
+							where tab.create_date = (
+														select max(sta.create_date)
+														from sys.schemas sch
+														join sys.tables sta
+															on sta.schema_id = sch.schema_id
+														where sch.name like 'backup_%'
+													)
+						)
+		and s.name like 'backup_%'
+
 				
 
 open table_cursor
@@ -63,14 +71,26 @@ DEALLOCATE table_cursor
 --Drop Schemas
 --===============================================================
 declare schema_cursor cursor for
-  select distinct name as schema_name
-            from sys.schemas
-			where name like 'backup_%'
-			and schema_id < (select max(schema_id)
-			                 from sys.schemas
-							 where name like 'backup_%'
-							 )
-				
+ 
+  select sc.name
+  from sys.schemas sc
+  where sc.name <> ( 
+  select sn.name
+  from sys.schemas sn
+  join sys.tables tab
+	on sn.schema_id = tab.schema_id
+  where tab.create_date = (
+							select max(sta.create_date)
+							from sys.schemas sch
+							join sys.tables sta
+								on sta.schema_id = sch.schema_id
+							where sch.name like 'backup_%'
+						  )
+					)
+					
+   and sc.name like 'backup_%'	
+   
+   								
 	open schema_cursor
 	fetch next from schema_cursor
 	into @schname
