fb243ff7609b10387d87d8d48dce10bafb9a1c55
YolindaMoodley <38342306+YolindaMoodley@users.noreply.github.com>
2018-07-31 12:35:31 +1000

    Incorporated changes to the Wingecarribee File which now has Assets on a single tab instead of multiple tabs

--- Build/Create Table Scripts/Staging/Aggregation/Wingercarribee.sql
+++ Build/Create Table Scripts/Staging/Aggregation/Wingercarribee.sql
@@ -27,7 +27,7 @@ create table staging.ContractSummary_Wingecarribee
 
 
 
-create table staging.AssetDetailsWBC_Wingecarribee
+create table staging.AssetDetails_Wingecarribee
 (
 [ACCOUNT_ID] nvarchar(50),
 [COLLATERAL_TYPE_DESC] nvarchar(50),
@@ -42,53 +42,15 @@ create table staging.AssetDetailsWBC_Wingecarribee
 
 )
 
-create table staging.AssetDetailsSBG_Wingecarribee
-(
-[Acct_Key] nvarchar(50),
-[Equipment_Type] nvarchar(50),
-[Description] nvarchar(100),
-[Make] nvarchar(100),
-[Model] nvarchar(100),
-[Year] nvarchar(4),
-[ASSET TYPE] nvarchar(50),
-[ASSET GROUP] nvarchar(1),
-[ASSET SUB GROUP] nvarchar(50),
-[FileDate] nvarchar(20)
-)
-
-
-create table staging.AssetDetailsCFAL_Wingecarribee
-(
-[Contract_Number] nvarchar(50),
-[Equipment_Type] nvarchar(50),
-[Description] nvarchar(100),
-[Make] nvarchar(50),
-[Model] nvarchar(100),
-[Year] nvarchar(4),
-[ASSET TYPE] nvarchar(100),
-[ASSET GROUP] nvarchar(1),
-[ASSET SUB GROUP] nvarchar(50),
-[FileDate] nvarchar(20)
-)
 
 create table staging.AssetDetailsNQ_Wingecarribee
 (
-[ACCOUNT_ID] nvarchar(50),
-[COLLATERAL_TYPE_DESC] nvarchar(50),
-[COLLATERAL_DESCRIPTION] nvarchar(100),
-[COLLATERAL_MAKE] nvarchar(50),
-[COLLATERAL_MODEL] nvarchar(100),
-[MANUFACTURE_YEAR] nvarchar(4),
-[ASSET TYPE] nvarchar(50),
-[ASSET GROUP] nvarchar(2),
-[ASSET SUB GROUP] nvarchar(50),
-[Rebate Month] nvarchar(50),
 [Brand] nvarchar(20),
 [Contract_Number] nvarchar(20),
 [Customer_ID] nvarchar(20),
 [Product_Type] nvarchar(50),
-[Discounted_Rate] nvarchar(6),
-[Undiscounted_Rate] nvarchar(6),
+[Discounted_Rate] nvarchar(10),
+[Undiscounted_Rate] nvarchar(10),
 [Repayment_Frequency] nvarchar(10),
 [Amount_Financed] nvarchar(15),
 [Contract_Term_Months] nvarchar(3),
