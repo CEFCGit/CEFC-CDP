7d83c1b654a2c60b249db949fcf4110141736486
YolindaMoodley <38342306+YolindaMoodley@users.noreply.github.com>
2018-08-27 10:07:41 +1000

    Logic to use Report as at date

--- Build/Stored Procedures/core.Aggregation_Report.sql
+++ Build/Stored Procedures/core.Aggregation_Report.sql
@@ -15,10 +15,8 @@ CREATE OR ALTER procedure [core].[Aggregation_Report] @file_date datetime
 as 
 
 	declare @local_date datetime
-	--declare @t_file_date nvarchar(20)
-  
 
-	select @local_date =  DBO.FN_LOCALDATE (GETDATE());
+  	select @local_date =  DBO.FN_LOCALDATE (GETDATE());
 
 	--select @t_file_date = EOMONTH(case when len(@file_date) = 6 then '01-'+@file_date else @file_date end); 
 
@@ -27,20 +25,14 @@ as
 	    (
 		select	max(prjd.Update_From_TS) b_Update_From_TS,
 		max(prjd.Update_to_TS) b_Update_To_TS,
-		max(l.Update_From_TS) l_Update_From_TS,
-		max(l.Update_to_TS) l_Update_To_TS,  
-	    prjd.ID_Project,
+		prjd.ID_Project,
 		prjd.Project_Name,
 		prjd.Description,
 		prjd.rpt_Project_Description,
 		prjd.Project_Borrower_Entity,
 		prjd.cOrganisations,
 		sum(Amt_CEFC) as Amt_CEFC,
-		l.Loc_State,
-	    l.Loc_Postcode,
-	    l.Loc_Electorate,
-	    l.Percent_of_Project_Amt,
-	    l.Percent_of_Project_tCO2_savings
+		prjd.ID_Project_TNumber
 		from core.Projects_Dimension prjd
 		join 
 		(select max(bdf.Update_From_TS) b_Update_From_TS,
@@ -65,103 +57,95 @@ as
 					from core.Aggregation_Fact a
 					join core.Projects_Dimension b
 					  on a.ID_Project = b.ID_Project
-					where EOMONTH(@file_date) >= convert(datetime,b.Update_From_TS) 
-					and EOMONTH(@file_date) <= convert(datetime,isnull(b.Update_to_TS,@local_date))
+					where EOMONTH(convert(datetime,@file_date)) >= convert(datetime,b.Update_From_TS) 
+					and EOMONTH(convert(datetime,@file_date)) <= convert(datetime,isnull(b.Update_to_TS,@local_date))
 					and a.File_Date = @file_date 
 					group by a.ID_Project,
 							 b.Project_Name  ) pd2
 				on pd.Project_Name like pd2.Project_Name+'%'
-				where EOMONTH(@file_date) >= convert(datetime,pd.Update_From_TS) 
-				and EOMONTH(@file_date) <= convert(datetime,isnull(pd.Update_to_TS,@local_date))
+				where EOMONTH(convert(datetime,@file_date)) >= convert(datetime,pd.Update_From_TS) 
+				and EOMONTH(convert(datetime,@file_date)) <= convert(datetime,isnull(pd.Update_to_TS,@local_date))
 				group by pd.ID_Project,
 						 pd.Project_Name) pd1
 			on bdf.ID_Project = pd1.ID_Project
-			where EOMONTH(@file_date) >= convert(datetime,bdf.Update_From_TS) 
-			and EOMONTH(@file_date) <= convert(datetime,isnull(bdf.Update_to_TS,@local_date))
+			where EOMONTH(convert(datetime,@file_date)) >= convert(datetime,bdf.Update_From_TS) 
+			and EOMONTH(convert(datetime,@file_date)) <= convert(datetime,isnull(bdf.Update_to_TS,@local_date))
 			group by case when charindex('II',pd1.Project_Name) > 0 then substring(pd1.Project_name,1,(charindex('II',pd1.Project_Name)-2)) else pd1.Project_Name end       
 			) as camt
 		on prjd.Project_Name = camt.Project_Name
-		left join core.Location_Dimension l
-	    on prjd.ID_Project = l.id_project
-		where EOMONTH(@file_date) >= convert(datetime,prjd.Update_From_TS) 
-		and EOMONTH(@file_date) <= convert(datetime,isnull(prjd.Update_to_TS,@local_date))
-		and EOMONTH(@file_date) >= convert(datetime,l.Update_From_TS)
-		and EOMONTH(@file_date) <= convert(datetime,isnull(l.Update_to_TS,@local_date))
+		where EOMONTH(convert(datetime,@file_date)) >= convert(datetime,prjd.Update_From_TS) 
+		and EOMONTH(convert(datetime,@file_date)) <= convert(datetime,isnull(prjd.Update_to_TS,@local_date))
 		group by prjd.ID_Project,
 				prjd.Project_Name,
 				prjd.Description,
 				prjd.rpt_Project_Description,
 				prjd.Project_Borrower_Entity,
 				prjd.cOrganisations,
-				l.Loc_State,
-				l.Loc_Postcode,
-				l.Loc_Electorate,
-			    l.Percent_of_Project_Amt,
-				l.Percent_of_Project_tCO2_savings
+				prjd.ID_Project_TNumber
 		) 
 
-    select af.[ID_Project]
-      ,af.[Contract_Number]
-      ,af.[Asset_Number]
-      ,af.[ANZSIC_Code]
-      ,af.[Balloon_Amount]
-      ,af.[Brand]
-      ,af.[CEFC_Rebate_Monthly]
-      ,af.[Contract_Start_Date]
-      ,af.[Discounted_Interest_Rate]
-      ,af.[Funder_Margin]
-      ,af.[Maturity_Date]
-      ,af.[Postcode]
-      ,af.[Principal_Outstanding]
-      ,af.[Product_Type]
-      ,af.[Repayment_Frequency]
-      ,af.[State]
-      ,af.[Term]
-      ,af.[Termination_Date]
-      ,af.[Total_Amount_Financed]
-      ,af.[Total_Rebate_to_date_exclGST]
-      ,af.[Total_Rebate_to_date_inclGST]
-      ,af.[File_Date]
-	  ,asd.[Category]
-	  ,asd.[Description] as Asset_Description
-	  ,asd.[Variant]
-	  ,asd.[Make]
-	  ,asd.[Model]
-	  ,asd.[Year]
-	  ,asd.[Status]
-	  ,ad.[Code]
-      ,ad.[Division]
-      ,ad.[Subdivision]
-      ,ad.[Description] as ANZSIC_Description
-	  ,ad.ABS_Division
-	  ,ad.ABS_Subdivision
-	  ,t.Amt_CEFC*t.Percent_of_Project_Amt as Amt_CEFC
-	  ,t.Project_Name
-	  ,t.cOrganisations
-	  ,t.Description as Project_Descriptiom
-	  ,t.rpt_Project_Description
-	  ,t.Project_Borrower_Entity
-	  ,t.Loc_State
-	  ,t.Loc_Postcode
-	  ,t.Loc_Electorate
-	  ,t.Percent_of_Project_Amt
-	  ,t.Percent_of_Project_tCO2_savings
+		
+		select af.[ID_Project]
+			  ,af.[Contract_Number]
+			  ,af.[Asset_Number]
+			  ,af.[ANZSIC_Code]
+			  ,af.[Balloon_Amount]
+			  ,af.[Brand]
+			  ,af.[CEFC_Rebate_Monthly]
+			  ,af.[Contract_Start_Date]
+			  ,af.[Discounted_Interest_Rate]
+			  ,af.[Funder_Margin]
+			  ,af.[Maturity_Date]
+			  ,af.[Postcode]
+			  ,af.[Principal_Outstanding]
+			  ,af.[Product_Type]
+			  ,af.[Repayment_Frequency]
+			  ,af.[State]
+			  ,af.[Term]
+			  ,af.[Termination_Date]
+			  ,af.[Total_Amount_Financed]
+			  ,af.[Total_Rebate_to_date_exclGST]
+			  ,af.[Total_Rebate_to_date_inclGST]
+			  ,asd.[Category]
+			  ,asd.[Description] as Asset_Description
+			  ,asd.[Variant]
+			  ,asd.[Make]
+			  ,asd.[Model]
+			  ,asd.[Year]
+			  ,asd.[Status]
+			  ,ad.[Code]
+			  ,ad.[Division]
+			  ,ad.[Subdivision]
+			  ,ad.[Description] as ANZSIC_Description
+			  ,ad.ABS_Division
+			  ,ad.ABS_Subdivision
+			  ,t.Amt_CEFC
+			  ,t.Project_Name
+			  ,t.cOrganisations
+			  ,t.Description as Project_Description
+			  ,t.rpt_Project_Description
+			  ,t.Project_Borrower_Entity
+			  ,t.ID_Project_TNumber  
+		from core.Aggregation_Fact af
+		join (select  max(convert(datetime,af1.File_Date)) as Agg_FileDate
+				,af1.[ID_Project]   	    	  
+				from core.Aggregation_Fact af1
+				where convert(datetime,af1.file_date) <= eomonth(format(@file_date,'yyyy-MM-dd'))   
+				group by af1.[ID_Project]) max_dates 
+		 on af.File_Date = max_dates.Agg_FileDate
+			and af.ID_Project = max_dates.ID_Project
+		left join [core].[Assets_Dimension] asd
+		 on af.ID_Project = asd.ID_Project
+		    and af.Contract_Number = asd.Contract_Number
+		    and af.Asset_Number = asd.Asset_Number
+		    and af.File_Date = asd.FileDate
+		left join core.ANZSIC_Dimension ad
+		 on af.ID_Project = ad.ID_Project
+		    and af.ANZSIC_Code = ad.Code
+		left join temp t
+		 on af.ID_Project = t.ID_Project    
+		     
 	  
-	from core.Aggregation_Fact af
-	left join [core].[Assets_Dimension] asd
-	  on af.ID_Project = asd.ID_Project
-	  and af.Contract_Number = asd.Contract_Number
-	  and af.Asset_Number = asd.Asset_Number
-	  and af.File_Date = asd.FileDate
-	left join core.ANZSIC_Dimension ad
-	  on af.ID_Project = ad.ID_Project
-	  and af.ANZSIC_Code = ad.Code
-    left join temp t
-	  on af.ID_Project = t.ID_Project    
-
-	where convert(datetime,af.File_Date) = @file_date
-	and EOMONTH(@file_date) >= convert(datetime,isnull(ad.Update_From_TS,@file_date))
-	and EOMONTH(@file_date) <= convert(datetime,isnull(ad.Update_to_TS,@local_date))
 	
 GO
 
